52928806d8a06b5cc007ff6f61efd74fc8044b6e
suomesta <shawn316michaels@gmail.com>
2019-05-09 23:10:04 +0900

    commit 009: add constructor with size and initial value

---
+++ 009/myvector.hpp
@@ -0,0 +1,251 @@
+/**
+ * @file    myvector.hpp
+ * @brief   Implementation of myvector class. 
+ *          myvector class imitates of std::vector. 
+ * @author  toda
+ * @date    2019-05-09
+ * @version 0.1.0
+ * @remark  the target is C++11 or more.
+ */
+
+#ifndef INCLUDE_GUARD_MYVECTOR_HPP
+#define INCLUDE_GUARD_MYVECTOR_HPP
+
+#include <cstddef>
+#include <iterator>
+#include <limits>
+#include <new>
+#include <stdexcept>
+
+/**
+ * @class   myvector
+ * @brief   myvector is a class which imitates std::vector.
+ * @tparam  T: The type of the elements.
+ */
+template <typename T>
+class myvector
+{
+public:
+    using value_type = T;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using iterator = value_type*;
+    using const_iterator = const value_type*;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    /////////////////////////////////////////////////////////////////////////////
+
+private:
+    enum : size_type { MAX_SIZE = std::numeric_limits<size_type>::max() };
+    /////////////////////////////////////////////////////////////////////////////
+
+public:
+    /**
+     * @brief      Default constructor. Constructs an empty container.
+     */
+    myvector(void) noexcept = default;
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Constructor with size and initial value. CConstructs the container
+     *             with count copies of elements with value.
+     * @param[in]  count: The size of the container.
+     * @param[in]  value: The value to initialize elements of the container with.
+     */
+    myvector(size_type count, const value_type& value) :
+        heap_(count ? mymalloc(length_check(count)) : nullptr),
+        size_(count),
+        capacity_(count)
+    {
+        for (size_type i = 0; i < size_; i++) {
+            new(&heap_[i]) value_type(value);
+        }
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Constructor with size. Constructs the container with count
+     *             default-inserted instances of T.
+     * @param[in]  count: The size of the container.
+     */
+    explicit myvector(size_type count) :
+        heap_(count ? mymalloc(length_check(count)) : nullptr),
+        size_(count),
+        capacity_(count)
+    {
+        for (size_type i = 0; i < size_; i++) {
+            new(&heap_[i]) value_type();
+        }
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Destructor. Destructs the vector.
+     */
+    ~myvector(void)
+    {
+        for (size_type i = 0; i < size_; i++) {
+            heap_[i].~value_type();
+        }
+        free(heap_);
+        heap_ = nullptr;
+        size_ = 0;
+        capacity_ = 0;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Returns an iterator to the first element of the container.
+     *             If the container is empty, the returned iterator will be equal
+     *             to end().
+     * @return     Iterator to the first element.
+     */
+    iterator begin(void) noexcept
+    {
+        return heap_;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Returns a const iterator to the first element of the container.
+     *             If the container is empty, the returned iterator will be equal
+     *             to end().
+     * @return     Const iterator to the first element.
+     */
+    const_iterator begin(void) const noexcept
+    {
+        return heap_;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Returns a const iterator to the first element of the container.
+     *             If the container is empty, the returned iterator will be equal
+     *             to cend().
+     * @return     Const iterator to the first element.
+     */
+    const_iterator cbegin(void) const noexcept
+    {
+        return heap_;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Returns an iterator to the element following the last element
+     *             of the container.
+     *             This element acts as a placeholder; attempting to access it
+     *             results in undefined behavior.
+     * @return     Iterator to the element following the last element.
+     */
+    iterator end(void) noexcept
+    {
+        return heap_ + size_;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Returns a const iterator to the element following the last element
+     *             of the container.
+     *             This element acts as a placeholder; attempting to access it
+     *             results in undefined behavior.
+     * @return     Const iterator to the element following the last element.
+     */
+    const_iterator end(void) const noexcept
+    {
+        return heap_ + size_;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Returns a const iterator to the element following the last element
+     *             of the container.
+     *             This element acts as a placeholder; attempting to access it
+     *             results in undefined behavior.
+     * @return     Const iterator to the element following the last element.
+     */
+    const_iterator cend(void) const noexcept
+    {
+        return heap_ + size_;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Returns the number of elements in the container.
+     * @return     The number of elements in the container.
+     */
+    size_type size(void) const noexcept
+    {
+        return size_;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Returns the maximum number of elements the container is able to
+     *             hold due to system or library implementation limitations, i.e.
+     *             std::distance(begin(), end()) for the largest container.
+     * @return     Maximum number of elements.
+     */
+    size_type max_size(void) const noexcept
+    {
+        return MAX_SIZE / sizeof(value_type);
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Returns the number of elements that the container has currently
+     *             allocated space for.
+     * @return     Capacity of the currently allocated storage.
+     */
+    size_type capacity(void) const noexcept
+    {
+        return capacity_;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+private:
+    /**
+     * @brief      Check new container size.
+     * @param[in]  count: Number of elements.
+     * @return     Same as count.
+     * @throw      std::length_error: If count is greater than the maximum size.
+     */
+    size_type length_check(size_type count) const
+    {
+        if (count > max_size()) {
+            throw std::length_error("myvecotr::length_check()");
+        }
+
+        return count;
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * @brief      Calls malloc with (sizeof(value_type) * count).
+     * @param[in]  count: Number of elements. Should be count > 0.
+     * @return     A pointer to the memory block allocated by malloc().
+     * @throw      std::bad_alloc: If malloc() fails to allocate storage.
+     */
+    pointer mymalloc(size_type count)
+    {
+        void* p = malloc(sizeof(value_type) * count);
+        if (p == nullptr) {
+            throw std::bad_alloc();
+        }
+
+        return static_cast<pointer>(p);
+    }
+    /////////////////////////////////////////////////////////////////////////////
+
+private:
+    pointer heap_ = nullptr;
+    size_type size_ = 0;
+    size_type capacity_ = 0;
+    /////////////////////////////////////////////////////////////////////////////
+};
+/////////////////////////////////////////////////////////////////////////////
+
+#endif // #ifndef INCLUDE_GUARD_MYVECTOR_HPP
---
+++ 009/myvector_test.cpp
@@ -0,0 +1,477 @@
+/**
+ * @file    myvector_test.cpp
+ * @brief   test of myvector using lest. 
+ * @author  toda
+ * @date    2019-05-09
+ * @version 0.1.0
+ * @remark  the target is C++11 or more.
+ */
+
+#include <type_traits>
+#include "../include/lest.hpp"
+#include "myvector.hpp"
+
+namespace {
+
+struct Noncopyable
+{
+    Noncopyable(void) = default;
+    Noncopyable(const Noncopyable&) = delete;
+    Noncopyable(Noncopyable&&) = default;
+    Noncopyable& operator=(const Noncopyable&) = delete;
+    Noncopyable& operator=(Noncopyable&&) = default;
+    ~Noncopyable(void) = default;
+};
+
+struct Unmovable
+{
+    Unmovable(void) = default;
+    Unmovable(const Unmovable&) = default;
+    Unmovable(Unmovable&&) = delete;
+    Unmovable& operator=(const Unmovable&) = default;
+    Unmovable& operator=(Unmovable&&) = delete;
+    ~Unmovable(void) = default;
+};
+
+template <typename T>
+struct is_const_lvalue_reference : std::conditional<std::is_lvalue_reference<T>::value && std::is_const<typename std::remove_reference<T>::type>::value,
+                                                    std::true_type,
+                                                    std::false_type>::type {};
+
+} // namespace {
+
+const lest::test specification[] =
+{
+
+    CASE("001: class definition")
+    {
+        // no actual tests
+        myvector<char> cv;
+        myvector<int> iv;
+        myvector<double> dv;
+
+        EXPECT(true);
+    },
+
+    CASE("002: public member types, and sizeof")
+    {
+        // check public member types for T is char, int, and double
+        {
+            using vec = myvector<char>;
+
+            EXPECT((std::is_same<char, vec::value_type>::value));
+            EXPECT((std::is_same<size_t, vec::size_type>::value));
+            EXPECT((std::is_same<ptrdiff_t, vec::difference_type>::value));
+            EXPECT((std::is_same<char&, vec::reference>::value));
+            EXPECT((std::is_same<const char&, vec::const_reference>::value));
+            EXPECT((std::is_same<char*, vec::pointer>::value));
+            EXPECT((std::is_same<const char*, vec::const_pointer>::value));
+            EXPECT((std::is_same<char*, vec::iterator>::value)); // not mandatory
+            EXPECT((std::is_same<const char*, vec::const_iterator>::value)); // not mandatory
+            EXPECT((std::is_same<std::reverse_iterator<char*>, vec::reverse_iterator>::value)); // not mandatory
+            EXPECT((std::is_same<std::reverse_iterator<const char*>, vec::const_reverse_iterator>::value)); // not mandatory
+        }
+        {
+            using vec = myvector<int>;
+
+            EXPECT((std::is_same<int, vec::value_type>::value));
+            EXPECT((std::is_same<size_t, vec::size_type>::value));
+            EXPECT((std::is_same<ptrdiff_t, vec::difference_type>::value));
+            EXPECT((std::is_same<int&, vec::reference>::value));
+            EXPECT((std::is_same<const int&, vec::const_reference>::value));
+            EXPECT((std::is_same<int*, vec::pointer>::value));
+            EXPECT((std::is_same<const int*, vec::const_pointer>::value));
+            EXPECT((std::is_same<int*, vec::iterator>::value)); // not mandatory
+            EXPECT((std::is_same<const int*, vec::const_iterator>::value)); // not mandatory
+            EXPECT((std::is_same<std::reverse_iterator<int*>, vec::reverse_iterator>::value)); // not mandatory
+            EXPECT((std::is_same<std::reverse_iterator<const int*>, vec::const_reverse_iterator>::value)); // not mandatory
+        }
+        {
+            using vec = myvector<double>;
+
+            EXPECT((std::is_same<double, vec::value_type>::value));
+            EXPECT((std::is_same<size_t, vec::size_type>::value));
+            EXPECT((std::is_same<ptrdiff_t, vec::difference_type>::value));
+            EXPECT((std::is_same<double&, vec::reference>::value));
+            EXPECT((std::is_same<const double&, vec::const_reference>::value));
+            EXPECT((std::is_same<double*, vec::pointer>::value));
+            EXPECT((std::is_same<const double*, vec::const_pointer>::value));
+            EXPECT((std::is_same<double*, vec::iterator>::value)); // not mandatory
+            EXPECT((std::is_same<const double*, vec::const_iterator>::value)); // not mandatory
+            EXPECT((std::is_same<std::reverse_iterator<double*>, vec::reverse_iterator>::value)); // not mandatory
+            EXPECT((std::is_same<std::reverse_iterator<const double*>, vec::const_reverse_iterator>::value)); // not mandatory
+        }
+
+        // check the result of sizeof(). Only minimum check is done
+        {
+            size_t minimum = sizeof(void*) + sizeof(size_t) + sizeof(size_t);
+
+            EXPECT(minimum <= sizeof(myvector<char>)); // not mandatory
+            EXPECT(minimum <= sizeof(myvector<int>)); // not mandatory
+            EXPECT(minimum <= sizeof(myvector<double>)); // not mandatory
+        }
+    },
+
+    CASE("003: default constructor")
+    {
+        // no actual tests
+        EXPECT_NO_THROW(myvector<char> v);
+        EXPECT_NO_THROW(myvector<int> v);
+        EXPECT_NO_THROW(myvector<double> v);
+    },
+
+    CASE("004: size()")
+    {
+        {
+            myvector<char> v;
+
+            EXPECT(0 == v.size());
+        }
+        {
+            myvector<int> v;
+
+            EXPECT(0 == v.size());
+        }
+        {
+            myvector<double> v;
+
+            EXPECT(0 == v.size());
+        }
+    },
+
+    CASE("005: capacity()")
+    {
+        {
+            myvector<char> v;
+
+            EXPECT(0 == v.capacity());
+        }
+        {
+            myvector<int> v;
+
+            EXPECT(0 == v.capacity());
+        }
+        {
+            myvector<double> v;
+
+            EXPECT(0 == v.capacity());
+        }
+    },
+
+    CASE("006: begin(), end(), cbegin(), cend()")
+    {
+        {
+            myvector<char> v;
+
+            EXPECT(nullptr == v.begin()); // not mandatory
+            EXPECT(nullptr == v.end()); // not mandatory
+            EXPECT(nullptr == v.cbegin()); // not mandatory
+            EXPECT(nullptr == v.cend()); // not mandatory
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(not is_const_lvalue_reference<decltype(*v.begin())>::value);
+            EXPECT(not is_const_lvalue_reference<decltype(*v.end())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cbegin())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cend())>::value);
+        }
+        {
+            const myvector<char> v;
+
+            EXPECT(nullptr == v.begin()); // not mandatory
+            EXPECT(nullptr == v.end()); // not mandatory
+            EXPECT(nullptr == v.cbegin()); // not mandatory
+            EXPECT(nullptr == v.cend()); // not mandatory
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(is_const_lvalue_reference<decltype(*v.begin())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.end())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cbegin())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cend())>::value);
+        }
+        {
+            myvector<int> v;
+
+            EXPECT(nullptr == v.begin()); // not mandatory
+            EXPECT(nullptr == v.end()); // not mandatory
+            EXPECT(nullptr == v.cbegin()); // not mandatory
+            EXPECT(nullptr == v.cend()); // not mandatory
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(not is_const_lvalue_reference<decltype(*v.begin())>::value);
+            EXPECT(not is_const_lvalue_reference<decltype(*v.end())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cbegin())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cend())>::value);
+        }
+        {
+            const myvector<int> v;
+
+            EXPECT(nullptr == v.begin()); // not mandatory
+            EXPECT(nullptr == v.end()); // not mandatory
+            EXPECT(nullptr == v.cbegin()); // not mandatory
+            EXPECT(nullptr == v.cend()); // not mandatory
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(is_const_lvalue_reference<decltype(*v.begin())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.end())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cbegin())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cend())>::value);
+        }
+        {
+            myvector<double> v;
+
+            EXPECT(nullptr == v.begin()); // not mandatory
+            EXPECT(nullptr == v.end()); // not mandatory
+            EXPECT(nullptr == v.cbegin()); // not mandatory
+            EXPECT(nullptr == v.cend()); // not mandatory
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(not is_const_lvalue_reference<decltype(*v.begin())>::value);
+            EXPECT(not is_const_lvalue_reference<decltype(*v.end())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cbegin())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cend())>::value);
+        }
+        {
+            const myvector<double> v;
+
+            EXPECT(nullptr == v.begin()); // not mandatory
+            EXPECT(nullptr == v.end()); // not mandatory
+            EXPECT(nullptr == v.cbegin()); // not mandatory
+            EXPECT(nullptr == v.cend()); // not mandatory
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(is_const_lvalue_reference<decltype(*v.begin())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.end())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cbegin())>::value);
+            EXPECT(is_const_lvalue_reference<decltype(*v.cend())>::value);
+        }
+    },
+
+    CASE("007: max_size()")
+    {
+        {
+            myvector<char> v;
+
+            using vec = myvector<char>;
+            vec::size_type expected = std::numeric_limits<vec::size_type>::max() / sizeof(vec::value_type);
+            EXPECT(expected == v.max_size()); // not mandatory
+        }
+        {
+            myvector<int> v;
+
+            using vec = myvector<int>;
+            vec::size_type expected = std::numeric_limits<vec::size_type>::max() / sizeof(vec::value_type);
+            EXPECT(expected == v.max_size()); // not mandatory
+        }
+        {
+            myvector<double> v;
+
+            using vec = myvector<double>;
+            vec::size_type expected = std::numeric_limits<vec::size_type>::max() / sizeof(vec::value_type);
+            EXPECT(expected == v.max_size()); // not mandatory
+        }
+    },
+
+    CASE("008: constructor with size, destructor")
+    {
+        {
+            myvector<char> v(3);
+
+            EXPECT(char() == *std::next(v.begin(), 0));
+            EXPECT(char() == *std::next(v.begin(), 1));
+            EXPECT(char() == *std::next(v.begin(), 2));
+            EXPECT(3 == std::distance(v.begin(), v.end()));
+            EXPECT(3 == std::distance(v.cbegin(), v.cend()));
+            EXPECT(3 == v.size());
+            EXPECT(3 == v.capacity());
+
+            v.~myvector(); // not mandatory
+
+            EXPECT(v.begin() == v.end()); // not mandatory
+            EXPECT(v.cbegin() == v.cend()); // not mandatory
+            EXPECT(0 == v.size()); // not mandatory
+            EXPECT(0 == v.capacity()); // not mandatory
+        }
+        {
+            myvector<int> v(3);
+
+            EXPECT(int() == *std::next(v.begin(), 0));
+            EXPECT(int() == *std::next(v.begin(), 1));
+            EXPECT(int() == *std::next(v.begin(), 2));
+            EXPECT(3 == std::distance(v.begin(), v.end()));
+            EXPECT(3 == std::distance(v.cbegin(), v.cend()));
+            EXPECT(3 == v.size());
+            EXPECT(3 == v.capacity());
+
+            v.~myvector(); // not mandatory
+
+            EXPECT(v.begin() == v.end()); // not mandatory
+            EXPECT(v.cbegin() == v.cend()); // not mandatory
+            EXPECT(0 == v.size()); // not mandatory
+            EXPECT(0 == v.capacity()); // not mandatory
+        }
+        {
+            myvector<double> v(3);
+
+            EXPECT(double() == *std::next(v.begin(), 0));
+            EXPECT(double() == *std::next(v.begin(), 1));
+            EXPECT(double() == *std::next(v.begin(), 2));
+            EXPECT(3 == std::distance(v.begin(), v.end()));
+            EXPECT(3 == std::distance(v.cbegin(), v.cend()));
+            EXPECT(3 == v.size());
+            EXPECT(3 == v.capacity());
+
+            v.~myvector(); // not mandatory
+
+            EXPECT(v.begin() == v.end()); // not mandatory
+            EXPECT(v.cbegin() == v.cend()); // not mandatory
+            EXPECT(0 == v.size()); // not mandatory
+            EXPECT(0 == v.capacity()); // not mandatory
+        }
+        {
+            myvector<char> v(0);
+
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(0 == v.size());
+            EXPECT(0 == v.capacity());
+        }
+        {
+            myvector<int> v(0);
+
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(0 == v.size());
+            EXPECT(0 == v.capacity());
+        }
+        {
+            myvector<double> v(0);
+
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(0 == v.size());
+            EXPECT(0 == v.capacity());
+        }
+        {
+            auto size = std::numeric_limits<myvector<int>::size_type>::max();
+            EXPECT_THROWS_AS((myvector<int>(size)), std::length_error);
+        }
+        {
+            auto size = std::numeric_limits<myvector<double>::size_type>::max();
+            EXPECT_THROWS_AS((myvector<double>(size)), std::length_error);
+        }
+        {
+            myvector<Noncopyable> v(1);
+
+            EXPECT(1 == std::distance(v.begin(), v.end()));
+            EXPECT(1 == std::distance(v.cbegin(), v.cend()));
+            EXPECT(1 == v.size());
+            EXPECT(1 == v.capacity());
+
+            v.~myvector(); // not mandatory
+
+            EXPECT(v.begin() == v.end()); // not mandatory
+            EXPECT(v.cbegin() == v.cend()); // not mandatory
+            EXPECT(0 == v.size()); // not mandatory
+            EXPECT(0 == v.capacity()); // not mandatory
+        }
+        {
+            myvector<Unmovable> v(1);
+
+            EXPECT(1 == std::distance(v.begin(), v.end()));
+            EXPECT(1 == std::distance(v.cbegin(), v.cend()));
+            EXPECT(1 == v.size());
+            EXPECT(1 == v.capacity());
+
+            v.~myvector(); // not mandatory
+
+            EXPECT(v.begin() == v.end()); // not mandatory
+            EXPECT(v.cbegin() == v.cend()); // not mandatory
+            EXPECT(0 == v.size()); // not mandatory
+            EXPECT(0 == v.capacity()); // not mandatory
+        }
+    },
+
+    CASE("009: constructor with size and initial value")
+    {
+        {
+            myvector<char> v(3, 'A');
+
+            EXPECT('A' == *std::next(v.begin(), 0));
+            EXPECT('A' == *std::next(v.begin(), 1));
+            EXPECT('A' == *std::next(v.begin(), 2));
+            EXPECT(3 == std::distance(v.begin(), v.end()));
+            EXPECT(3 == std::distance(v.cbegin(), v.cend()));
+            EXPECT(3 == v.size());
+            EXPECT(3 == v.capacity());
+        }
+        {
+            myvector<int> v(3, 10);
+
+            EXPECT(10 == *std::next(v.begin(), 0));
+            EXPECT(10 == *std::next(v.begin(), 1));
+            EXPECT(10 == *std::next(v.begin(), 2));
+            EXPECT(3 == std::distance(v.begin(), v.end()));
+            EXPECT(3 == std::distance(v.cbegin(), v.cend()));
+            EXPECT(3 == v.size());
+            EXPECT(3 == v.capacity());
+        }
+        {
+            myvector<double> v(3, 1.5);
+
+            EXPECT(1.5 == *std::next(v.begin(), 0));
+            EXPECT(1.5 == *std::next(v.begin(), 1));
+            EXPECT(1.5 == *std::next(v.begin(), 2));
+            EXPECT(3 == std::distance(v.begin(), v.end()));
+            EXPECT(3 == std::distance(v.cbegin(), v.cend()));
+            EXPECT(3 == v.size());
+            EXPECT(3 == v.capacity());
+        }
+        {
+            myvector<char> v(0, 'A');
+
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(0 == v.size());
+            EXPECT(0 == v.capacity());
+        }
+        {
+            myvector<int> v(0, 10);
+
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(0 == v.size());
+            EXPECT(0 == v.capacity());
+        }
+        {
+            myvector<double> v(0, 1.5);
+
+            EXPECT(v.begin() == v.end());
+            EXPECT(v.cbegin() == v.cend());
+            EXPECT(0 == v.size());
+            EXPECT(0 == v.capacity());
+        }
+        {
+            auto size = std::numeric_limits<myvector<int>::size_type>::max();
+            EXPECT_THROWS_AS((myvector<int>(size, 10)), std::length_error);
+        }
+        {
+            auto size = std::numeric_limits<myvector<double>::size_type>::max();
+            EXPECT_THROWS_AS((myvector<double>(size, 1.5)), std::length_error);
+        }
+        {
+            myvector<Unmovable> v(1, Unmovable());
+
+            EXPECT(1 == std::distance(v.begin(), v.end()));
+            EXPECT(1 == std::distance(v.cbegin(), v.cend()));
+            EXPECT(1 == v.size());
+            EXPECT(1 == v.capacity());
+        }
+    },
+
+};
+
+int main(int argc, char* argv[])
+{
+    return lest::run(specification, argc, argv);
+}
+/////////////////////////////////////////////////////////////////////////////
